61f4599ad3e750579ae5fb6f1ad6284e4853f61d
xor111 <139008173+xor111@users.noreply.github.com>
2023-10-10 12:08:37 -0600

    Create UREQ.rb

---
+++ Casks/UREQ.rb
@@ -0,0 +1,10 @@
+cask 'UREQ' do
+  version :latest
+  sha256 :no_check
+
+  url "https://analogobsession.com/wp-content/uploads/2023/08/UREQ_1.0.pkg"
+  name 'UREQ'
+  homepage 'https://www.patreon.com/posts/88488633?pr=true'
+
+  pkg 'UREQ_1.0.pkg'
+end
